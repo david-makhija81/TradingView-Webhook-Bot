3cc10a2760a9c5f618426adffac4788be37b191e
david-makhija81 <119004438+david-makhija81@users.noreply.github.com>
2026-06-10 17:20:11 +0530

    Add runtime logging for webhook alerts

--- handler.py
+++ handler.py
@@ -4,6 +4,7 @@
 # File Name             : handler.py              #
 # ----------------------------------------------- #
 
+import logging
 import smtplib
 import ssl
 from email.mime.text import MIMEText
@@ -15,83 +16,129 @@ from telegram import Bot
 
 import config
 
+logger = logging.getLogger(__name__)
+
+
+def _sanitize_alert_message(message):
+    return message.replace("*", "").replace("_", "").replace("`", "")
+
 
 def send_alert(data):
     msg = data["msg"].encode("latin-1", "backslashreplace").decode("unicode_escape")
+    logger.info("Starting alert dispatch with message length %s", len(msg))
+
     if config.send_telegram_alerts:
-        tg_bot = Bot(token=config.tg_token)
+        target_channel = data.get("telegram", config.channel)
+        target_source = "payload-provided" if "telegram" in data else "configured"
+        logger.info(
+            "Telegram alerts enabled; sending notification to %s channel %s",
+            target_source,
+            target_channel,
+        )
         try:
+            tg_bot = Bot(token=config.tg_token)
             tg_bot.sendMessage(
-                data["telegram"],
+                target_channel,
                 msg,
                 parse_mode="MARKDOWN",
             )
-        except KeyError:
-            tg_bot.sendMessage(
-                config.channel,
-                msg,
-                parse_mode="MARKDOWN",
+            logger.info(
+                "Telegram notification sent successfully to %s channel %s",
+                target_source,
+                target_channel,
             )
-        except Exception as e:
-            print("[X] Telegram Error:\n>", e)
+        except Exception:
+            logger.exception(
+                "Telegram notification failed for %s channel %s",
+                target_source,
+                target_channel,
+            )
+    else:
+        logger.debug("Telegram alerts disabled; skipping Telegram notification")
 
     if config.send_discord_alerts:
+        webhook_path = data.get("discord", config.discord_webhook)
+        target_source = "payload-provided" if "discord" in data else "configured"
+        logger.info(
+            "Discord alerts enabled; sending notification using %s webhook",
+            target_source,
+        )
         try:
             webhook = DiscordWebhook(
-                url="https://discord.com/api/webhooks/" + data["discord"]
+                url="https://discord.com/api/webhooks/" + webhook_path
             )
             embed = DiscordEmbed(title=msg)
             webhook.add_embed(embed)
             webhook.execute()
-        except KeyError:
-            webhook = DiscordWebhook(
-                url="https://discord.com/api/webhooks/" + config.discord_webhook
+            logger.info(
+                "Discord notification sent successfully using %s webhook", target_source
             )
-            embed = DiscordEmbed(title=msg)
-            webhook.add_embed(embed)
-            webhook.execute()
-        except Exception as e:
-            print("[X] Discord Error:\n>", e)
+        except Exception:
+            logger.exception(
+                "Discord notification failed using %s webhook", target_source
+            )
+    else:
+        logger.debug("Discord alerts disabled; skipping Discord notification")
 
     if config.send_slack_alerts:
+        webhook_path = data.get("slack", config.slack_webhook)
+        target_source = "payload-provided" if "slack" in data else "configured"
+        logger.info(
+            "Slack alerts enabled; sending notification using %s webhook", target_source
+        )
         try:
-            slack = Slack(url="https://hooks.slack.com/services/" + data["slack"])
+            slack = Slack(url="https://hooks.slack.com/services/" + webhook_path)
             slack.post(text=msg)
-        except KeyError:
-            slack = Slack(
-                url="https://hooks.slack.com/services/" + config.slack_webhook
+            logger.info(
+                "Slack notification sent successfully using %s webhook", target_source
             )
-            slack.post(text=msg)
-        except Exception as e:
-            print("[X] Slack Error:\n>", e)
+        except Exception:
+            logger.exception(
+                "Slack notification failed using %s webhook", target_source
+            )
+    else:
+        logger.debug("Slack alerts disabled; skipping Slack notification")
 
     if config.send_twitter_alerts:
-        tw_auth = tweepy.OAuthHandler(config.tw_ckey, config.tw_csecret)
-        tw_auth.set_access_token(config.tw_atoken, config.tw_asecret)
-        tw_api = tweepy.API(tw_auth)
+        logger.info("Twitter alerts enabled; sending status update")
         try:
-            tw_api.update_status(
-                status=msg.replace("*", "").replace("_", "").replace("`", "")
-            )
-        except Exception as e:
-            print("[X] Twitter Error:\n>", e)
+            tw_auth = tweepy.OAuthHandler(config.tw_ckey, config.tw_csecret)
+            tw_auth.set_access_token(config.tw_atoken, config.tw_asecret)
+            tw_api = tweepy.API(tw_auth)
+            tw_api.update_status(status=_sanitize_alert_message(msg))
+            logger.info("Twitter status update sent successfully")
+        except Exception:
+            logger.exception("Twitter status update failed")
+    else:
+        logger.debug("Twitter alerts disabled; skipping Twitter status update")
 
     if config.send_email_alerts:
+        logger.info(
+            "Email alerts enabled; sending notification to %s recipient(s)",
+            len(config.email_receivers),
+        )
         try:
-            email_msg = MIMEText(
-                msg.replace("*", "").replace("_", "").replace("`", "")
-            )
+            email_msg = MIMEText(_sanitize_alert_message(msg))
             email_msg["Subject"] = config.email_subject
             email_msg["From"] = config.email_sender
             email_msg["To"] = config.email_sender
             context = ssl.create_default_context()
+            logger.info(
+                "Connecting to email server %s:%s", config.email_host, config.email_port
+            )
             with smtplib.SMTP_SSL(
                 config.email_host, config.email_port, context=context
             ) as server:
                 server.login(config.email_user, config.email_password)
+                logger.info("Authenticated to email server")
                 server.sendmail(
                     config.email_sender, config.email_receivers, email_msg.as_string()
                 )
                 server.quit()
-        except Exception as e:
-            print("[X] Email Error:\n>", e)
+            logger.info("Email notification sent successfully")
+        except Exception:
+            logger.exception("Email notification failed")
+    else:
+        logger.debug("Email alerts disabled; skipping email notification")
+
+    logger.info("Alert dispatch finished")
